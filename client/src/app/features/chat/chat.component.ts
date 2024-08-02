46d07021aa95b18762769bd94618de4b893094c3
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2024-08-03 02:55:26 +0300

    Modify the chat component, reorganize it.
    - Change the HTML page along with the CSSto be a little UI friendly (not final version).
    - Add the create room and join room functions.
    - Create a new Room Subscription to get the live updates.

---
+++ client/src/app/features/chat/chat.component.ts
@@ -0,0 +1,93 @@
+import {Component, Inject, OnDestroy, OnInit, PLATFORM_ID} from '@angular/core';
+import {WebsocketService} from "../../core/services/websocket.service";
+import {Subscription} from 'rxjs';
+import {isPlatformBrowser} from "@angular/common";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+  message: string = '';
+  messages: string[] = [];
+  rooms: string[] = [];
+  newRoomName: string = '';
+  currentRoom: string = 'main';
+
+  private messageSubscription: Subscription = new Subscription();
+  private roomSubscription: Subscription = new Subscription();
+
+  constructor(
+    private chatService: WebsocketService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.messageSubscription = this.chatService.message$.subscribe({
+      next: (message) => {
+        this.messages.push(message);
+      }, error: (error) => {
+        console.error('WebSocket error:', error);
+      }, complete: () => {
+        console.warn('WebSocket connection closed');
+      }
+    })
+    this.roomSubscription = this.chatService.rooms$.subscribe({
+      next: (room) => {
+        this.rooms = room;
+      }, error: (error) => {
+        console.error('WebSocket error:', error);
+      }, complete: () => {
+        console.warn('WebSocket connection closed');
+      }
+    })
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.connect();
+    }
+
+  }
+
+  connect(): void {
+    this.chatService.connect()
+      .then(() => {
+        this.listRooms();
+      })
+      .catch((error) => {
+        console.error('WebSocket connection failed:', error);
+      });
+  }
+
+  sendMessage(): void {
+    this.chatService.sendMessage(this.message);
+    this.message = '';
+  }
+
+  listRooms(): void {
+    console.log('Listing rooms');
+    this.chatService.listRooms()
+  }
+
+  joinRoom(room: string): void {
+    if (room !== this.currentRoom) {
+      this.chatService.joinRoom(room);
+      this.currentRoom = room;
+    }
+  }
+
+  createRoom(): void {
+    if (this.newRoomName.trim() && this.newRoomName !== this.currentRoom) {
+      this.joinRoom(this.newRoomName.trim());
+      this.newRoomName = '';
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.messageSubscription.unsubscribe();
+    this.roomSubscription.unsubscribe();
+  }
+}
