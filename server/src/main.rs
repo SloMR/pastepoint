173167578a961b36d9c039c612999384eba30b96
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2024-08-12 03:00:01 +0300

    Create docker container for the project.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use actix_web::{
     get, middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
 };
@@ -22,18 +24,29 @@ async fn chat_ws(req: HttpRequest, stream: web::Payload) -> Result<impl Responde
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
-    log::debug!("starting HTTPS server at https://127.0.0.1:9000");
+    log::debug!("starting HTTPS server at https://0.0.0.0:9000");
 
-    // load TLS keys
-    // to create a self-signed temporary cert for testing:
-    // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
+    let environment = env::var("RUN_ENV").unwrap_or_else(|_| "development".to_string());
+
+    let (key_file, cert_file) = if environment == "production" {
+        (
+            "/etc/ssl/certs/key.pem".to_string(),
+            "/etc/ssl/certs/cert.pem".to_string(),
+        )
+    } else {
+        (
+            "certs/key.pem".to_string(),
+            "certs/cert.pem".to_string(),
+        )
+    };
+
+    // Load TLS keys
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file("certs/key.pem", SslFiletype::PEM)
-        .unwrap();
-    builder
-        .set_certificate_chain_file("certs/cert.pem")
-        .unwrap();
+    builder.set_private_key_file(key_file.clone(), SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file(cert_file.clone()).unwrap();
+
+    log::debug!("Using key file: {}", key_file);
+    log::debug!("Using cert file: {}", cert_file);
 
     HttpServer::new(move || {
         App::new()
@@ -44,7 +57,7 @@ async fn main() -> std::io::Result<()> {
             .service(chat_ws)
             .wrap(Logger::default())
     })
-    .bind_openssl("127.0.0.1:9000", builder)?
+    .bind_openssl("0.0.0.0:9000", builder)?
     .run()
     .await
 }
