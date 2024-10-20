0817034db53580d3e35cf1867477e441ded4e6d5
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2024-10-20 15:12:00 +0300

    Create user service.
    - Handle the user name.

---
+++ client/src/app/core/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private userSubject = new BehaviorSubject<string>('');
+  public user$ = this.userSubject.asObservable();
+
+  constructor() {}
+
+  public get user(): string {
+    return this.userSubject.value;
+  }
+
+  public set user(value: string) {
+    this.userSubject.next(value);
+  }
+}
