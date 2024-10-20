ecfcaaf9e8e5d7b7d7b3130cfdeb42c5ebf50b2a
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2024-10-20 15:16:41 +0300

    Create Chat Service.
    - We will handle all the chat related services such as sending messages, getting the user name and system messages.

--- client/src/app/core/services/chat.service.spec.ts
+++ client/src/app/core/services/chat.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { WebsocketService } from './websocket.service';
+import { ChatService } from './chat.service';
 
-describe('WebsocketService', () => {
-  let service: WebsocketService;
+describe('FileTransferService', () => {
+  let service: ChatService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(WebsocketService);
+    service = TestBed.inject(ChatService);
   });
 
   it('should be created', () => {
---
+++ client/src/app/core/services/chat.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { LoggerService } from './logger.service';
+import { WebSocketConnectionService } from './websocket-connection.service';
+import { WebRTCService } from './webrtc.service';
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  public messages$ = new BehaviorSubject<string[]>([]);
+  private messages: string[] = [];
+
+  constructor(
+    private logger: LoggerService,
+    private wsService: WebSocketConnectionService,
+    private webrtcService: WebRTCService,
+    private userService: UserService
+  ) {
+    this.webrtcService.chatMessages$.subscribe((message) => {
+      this.handleUserMessage(message);
+    });
+
+    this.wsService.systemMessages$.subscribe((message) => {
+      this.handleSystemMessage(message);
+    });
+  }
+
+  public get user(): string {
+    return this.userService.user;
+  }
+
+  public set user(value: string) {
+    this.userService.user = value;
+  }
+
+  public sendMessage(content: string, targetUser: string): void {
+    if (content.trim()) {
+      const message = {
+        type: 'chat',
+        payload: `${this.user}: ${content.trim()}`,
+      };
+      this.webrtcService.sendData(message, targetUser);
+      this.messages.push(`${this.user}: ${content.trim()}`);
+      this.messages$.next(this.messages);
+    }
+  }
+
+  private handleUserMessage(message: string): void {
+    this.messages.push(message);
+    this.messages$.next(this.messages);
+  }
+
+  private handleSystemMessage(message: string): void {
+    if (message.includes('[SystemName]')) {
+      const matchName = message.match(/\[SystemName\]\s*:\s*(.*?)$/);
+      if (matchName && matchName[1]) {
+        const userName = matchName[1].trim();
+        this.logger.log(`Username updated from: ${this.user}, to: ${userName}`);
+        this.user = userName;
+      }
+    }
+  }
+
+  public getUsername(): void {
+    this.wsService.send('[UserCommand] /name');
+  }
+}
