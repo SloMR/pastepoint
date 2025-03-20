6998d1250ef2c7670109356c47cdb6fcf9351617
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2025-03-20 20:16:17 +0300

    Client : Configuration Refinements
    - Reordered environment variables in .env.development for better readability.
    - Specified version for gettext in client Dockerfile to ensure consistent builds.
    - Changed user from root to nginx in client Dockerfile, enhancing security by avoiding running as root.
    - Adjusted syntax of envsubst command in docker-entrypoint.sh for clarity and consistency with best practices.

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { NgxLoggerLevel } from 'ngx-logger';
 
 export const environment = {
   production: false,
-  apiUrl: '0.0.0.0:9000',
+  apiUrl: '127.0.0.1:9000',
   logLevel: NgxLoggerLevel.DEBUG,
   enableSourceMaps: true,
   disableFileDetails: false,
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst "\$SERVER_NAME \$SSL_CERT_PATH \$SSL_CERT_KEY_PATH" < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst "\$SERVER_NAME \$SSL_CERT_PATH \$SSL_CERT_KEY_PATH" </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
 echo "Starting Nginx..."
 
 exec "$@"
